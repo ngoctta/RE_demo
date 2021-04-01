2f7d37845602b03a73231a304e95bd3525bad798
ngoctta12@gmail.com <ngoctta12@gmail.com>
2021-04-01 15:18:01 +0700

    update js cloudimage 360

--- src/chooseCarColor/ChooseCarColor.js
+++ src/chooseCarColor/ChooseCarColor.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import "./style.css";
 import wheels from './wheels.json';
 import features from './arrayFeatures.json';
-import xoay from 'js-cloudimage-360-view';
 
 export default class ChooseCarColor extends Component {
     state = {
@@ -12,7 +11,7 @@ export default class ChooseCarColor extends Component {
     renderColor = () => {
         return features.map((item, index) => {
             return (
-                 <div className={`border mb-3 ${ this.state.idCar == item.id ? " border-warning" : ""}`} key={index}>
+                 <div className={`border mb-3 ${ this.state.idCar === item.id ? " border-warning" : ""}`} key={index}>
                     <div className="d-flex p-2" style={{ cursor: "pointer"}} key={ item.id } onClick={() => {this.handleChooseColor(item.id)}}>
                         <div style={{ width: "60px", marginRight: "10px"}}>
                             <img src={item.img} className="img-fluid w-100"/>
@@ -35,7 +34,7 @@ export default class ChooseCarColor extends Component {
     renderWheels = () => {
         return wheels.map((item, index) => {
             return ( 
-            <div className={`border mb-3 ${ this.state.idWheel == item.idWheel ? " border-warning" : ""}`} key={index}>
+            <div className={`border mb-3 ${ this.state.idWheel === item.idWheel ? " border-warning" : ""}`} key={index}>
                 <div className="d-flex p-2" style={{ cursor: "pointer"}} onClick={() => {this.handleChooseWheels(item.idWheel)}}>
                     <div style={{ width: "60px", flexShrink: 0, marginRight: "10px"}}>
                         <img src={ item.img } className="img-fluid w-100"/>
@@ -49,9 +48,22 @@ export default class ChooseCarColor extends Component {
         })
     }
     getFolderImage = () => {
-        let car = features.filter(item => item.id == this.state.idCar)[0];
-        let wheelItem = car.wheels.filter(item => item.idWheel == this.state.idWheel)[0];
+        let car = features.filter(item => item.id === this.state.idCar)[0];
+        let wheelItem = car.wheels.filter(item => item.idWheel === this.state.idWheel)[0];
         return "./images/" + wheelItem.srcImg;
+    }
+    componentDidMount = () => {
+        let script = document.createElement("script");
+        script.src = "https://cdn.scaleflex.it/plugins/js-cloudimage-360-view/2.6.0/js-cloudimage-360-view.min.js";       
+        let el = document.getElementById("append-cloudimage").appendChild(script);
+    };
+    componentDidUpdate = () => {
+        document.getElementById("applycanvas").innerHTML = "";
+        let el = document.getElementById("append-cloudimage");
+        el.innerHTML =""
+        let script = document.createElement("script");
+        script.src = "https://cdn.scaleflex.it/filerobot/js-cloudimage-360-view/v2.0.0.lazysizes.min.js";       
+        el.appendChild(script);
     }
 
     render() {
@@ -61,7 +73,9 @@ export default class ChooseCarColor extends Component {
                     <div className="col-md-7">
                         <div className="border">
                             {/* <div className="cloudimage-360" data-folder= data-filename="civic-{index}.jpg" data-amount={8} /> */}
-                        <div className="cloudimage-360" data-folder="./images/images-black/images-black-1/" data-filename="civic-{index}.jpg" data-amount={8} />
+                            <div id="applycanvas" className="cloudimage-360" data-folder={ this.getFolderImage() } data-filename="civic-{index}.jpg" data-amount={8} />
+                        </div>
+                        <div id="append-cloudimage">
 
                         </div>
                     </div>
